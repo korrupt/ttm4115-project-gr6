e3cf0908c30d690513f28a4846b819a7438ac715
korrupt <k0rrupt.dev@gmail.com>
2024-03-11 15:35:10 +0100

    added domain feature

---
+++ libs/nest/charger/data-access/domain/README.md
@@ -0,0 +1,7 @@
+# nest-charger-data-access-domain
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test nest-charger-data-access-domain` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/nest/charger/data-access/domain/src/lib/charger.ts
@@ -0,0 +1,23 @@
+
+
+export interface ChargerRepository {
+  save: (charger: Charger) => Promise<Charger>;
+}
+
+export interface ChargerProps {
+  readonly id: string;
+  name: string;
+  location: [number, number];
+}
+
+export class Charger implements ChargerProps {
+  readonly id!: string;
+  name!: string;
+  location!: [number, number];
+
+  constructor(props: ChargerProps){
+    Object.assign(this, props);
+  }
+}
+
+
