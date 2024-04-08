3dea6f3a6d0e93230c81abe4296438e5eaffe1d6
korrupt <k0rrupt.dev@gmail.com>
2024-04-08 22:23:45 +0200

    feat: added validation pipe

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,6 +9,7 @@ import {
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CqrsModule } from '@nestjs/cqrs';
 import { NestChargerFeatureModule } from '@prosjekt/nest/charger/feature';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -30,6 +31,12 @@ import { NestChargerFeatureModule } from '@prosjekt/nest/charger/feature';
     NestChargerFeatureModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({ transform: true, whitelist: true })
+    }
+  ],
 })
 export class AppModule {}
