d77c26211c52ba3791dd3d32a724b08d586b8aaf
Samund <samunde@stud.ntnu.no>
2024-04-15 15:41:22 +0200

    Charger with non-functioning MQTT-client

--- stmpy/charger.py
+++ stmpy/charger.py
@@ -3,11 +3,11 @@ import logging
 import paho.mqtt.client as mqtt
 from threading import Thread
 
-broker, port = "ibsen.no", 1883
+broker, port = "ipsen.dev", 1883
 
 class Charger:
     def __init__(self):
-        pass
+        self.electricity = 0
 
     def chargingStarted(self):
         self.stm.send("start")
@@ -28,8 +28,27 @@ class Charger:
         self.stm.send("repaired")
         logging.info("Charger fixed")
 
-    def logStarted(self):
-        logging.info("Actually started")
+    def start_measure_electricity(self):
+        self.electricity = 0
+
+    def end_measure_electricity(self):
+        self.electricity = 15
+        return self.electricity
+    
+    def msg_cloud(self, msg):
+        if (msg == "idle"):
+            print("idle_msg")
+        elif (msg == "active"):
+            print("active_msg")
+        elif (msg == "done"):
+            print("done_msg_with_consumption", self.electricity)
+        elif (msg == "down"):
+            print("down_msg")
+        elif (msg == "repaired"):
+            print("repaired_msg")
+        else:
+            print("error, no msg of this type")
+
 
 class MQTT_Client_1:
     def __init__(self):
@@ -43,16 +62,17 @@ class MQTT_Client_1:
  
     def on_message(self, client, userdata, msg):
         topic = msg.topic
-        if (topic == "quiz/master"):
-            self.stm_driver.send("quiz_master_message", "quiz")
+        print(topic)
+        if (topic == "charger/start"):
+            self.stm_driver.send("start", "stm_charger")
         else:
-            self.stm_driver.send("ack", "quiz")
- 
+            print("msg_unsupported, ", topic)
+        
  
     def start(self, broker, port):
         print("Connecting to {}:{}".format(broker, port))
         self.client.connect(broker, port)
-        self.client.subscribe("quiz/+")
+        self.client.subscribe("charger/+")
  
         try:
             # line below should not have the () after the function!
@@ -62,16 +82,13 @@ class MQTT_Client_1:
             print("Interrupted")
             self.client.disconnect()
 
-charger = Charger()
-
 t_init = {'source': 'initial',
         'target': 'idle'
       }
 
 t_start = {'trigger': 'start',
           'source': 'idle',
-          'target': 'active',
-          'effect': 'logStarted()'
+          'target': 'active'
           }
 
 t_end = {'trigger': 'end',
@@ -94,7 +111,27 @@ t_repaired = {'trigger': 'repaired',
               'target': 'idle'
               }
 
-stm_charger = Machine(name='stm_charger', transitions=[t_init, t_start, t_end, t_idle_down, t_active_down, t_repaired], obj=charger)
+s_idle = {
+    'name': 'idle',
+    'entry': 'msg_cloud("idle")'
+}
+
+s_active = {
+    'name': 'active',
+    'entry': 'msg_cloud("active");start_measure_electricity()',
+    'exit': 'end_measure_electricity();msg_cloud("done")'
+
+}
+
+s_down = {
+    'name': 'down',
+    'entry': 'msg_cloud("down")',
+    'exit' : 'msg_cloud("repaired")'
+}
+
+
+charger = Charger()
+stm_charger = Machine(name='stm_charger', states=[s_idle, s_active, s_down] ,transitions=[t_init, t_start, t_end, t_idle_down, t_active_down, t_repaired], obj=charger)
 charger.stm = stm_charger
 
 logger = logging.getLogger('stmpy.Driver')
@@ -117,6 +154,11 @@ logging.getLogger().setLevel(logging.INFO)
     
 driver = Driver()
 driver.add_machine(stm_charger)
+
+myclient = MQTT_Client_1()
+charger.mqtt_client = myclient.client
+myclient.stm_driver = driver
+
 driver.start()
 
 def test_charger():
@@ -126,6 +168,6 @@ def test_charger():
     charger.chargerRepaired()
     charger.chargingStarted()
     charger.chargerDown()
-    print("Done")
-
-test_charger()
+    print("Testing_cycle_down")
+print("program did not crash")
+# test_charger()
